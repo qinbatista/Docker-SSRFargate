fcaa6b79f60b4c7c72c93485955302b34d37bcfe
MacStudio <qin.batista@outlook.com>
2022-11-01 23:21:14 -0700

    update

--- ECSManager.py
+++ ECSManager.py
@@ -105,3 +105,6 @@ class ECSManager:
                 return result["taskArns"][0]
         except Exception as e:
             self.__log(f"[_stop_task] failed:" + str(e))
+if __name__ == "__main__":
+    ss = ECSManager()
+    ss._replace_fargate()
