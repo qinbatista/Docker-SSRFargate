5da8ba10332a5d61d3073c359c053bfa1c89ece6
qin.batista <qin.batista@outlook.com>
2023-03-02 10:44:27 -0800

    update

--- DiscordChatGPT/App/OpenAILib/OpenAI.py
+++ DiscordChatGPT/App/OpenAILib/OpenAI.py
@@ -7,7 +7,7 @@ openai.api_key = os.getenv("CHATGPT_API_KEY")
 
 def chatgpt_response(prompt):
     response =  openai.Completion.create(
-        model = "text-davinci-003",
+        model = "gpt-3.5-turbo",
         prompt = prompt,
         temperature = 1,
         max_tokens = 4000,
